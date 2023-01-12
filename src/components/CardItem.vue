7848e45b84c75726f53496f7bbd8678355e4b1ee
Jafar <tljafer@gmail.com>
2023-01-12 16:46:06 +0400

    removed svg icon

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -6,14 +6,6 @@
             :class="{ 'card-mini-dark': isDarkMode, 'card-mini-list-scroll': item.child_items }"
             :style="[item.is_toggle_btn && item.enabled === false && { 'background-color': '#ffffff2e' }]">
             <div class="card-body card-mini">
-                <div class="card-mini-icon text-grey d-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15.384" height="15.384" viewBox="0 0 15.384 15.384" fill="currentColor">
-                        <g>
-                            <path d="M0,0H15.384V15.384H0Z" fill="none" />
-                            <path d="M1.652,9.629A5.64,5.64,0,1,1,9.629,1.652,5.64,5.64,0,1,1,1.652,9.629ZM.9,5.641A4.738,4.738,0,1,0,5.641.9,4.743,4.743,0,0,0,.9,5.641ZM5.128,7.692V5.128a.513.513,0,0,1,1.025,0V7.692a.513.513,0,1,1-1.025,0Zm0-4.1a.513.513,0,1,1,.513.513A.513.513,0,0,1,5.128,3.59Z" transform="translate(2.051 2.051)" />
-                        </g>
-                    </svg>
-                </div>
                 <div class="d-flex justify-content-between card-mini-title-wrapper">
                     <div class="card-mini-title text-grey">{{ item.label }}</div>
                     <label v-if="item.is_toggle_btn" class="switch" :for="'card-mini-checkbox-' + index">
--- src/components/LongCardMini.vue
+++ src/components/LongCardMini.vue
@@ -9,14 +9,6 @@
                         <div class="card-mini-title text-grey" :style="[item.logo && { fontWeight: '600', fontSize: '100%' }]">{{ item.label }}</div>
                         <div class="card-mini-amount mt-2">{{ item.amount }}</div>
                     </div>
-                    <div class="card-mini-icon text-grey d-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="15.384" height="15.384" viewBox="0 0 15.384 15.384" fill="currentColor">
-                            <g>
-                                <path d="M0,0H15.384V15.384H0Z" fill="none" />
-                                <path d="M1.652,9.629A5.64,5.64,0,1,1,9.629,1.652,5.64,5.64,0,1,1,1.652,9.629ZM.9,5.641A4.738,4.738,0,1,0,5.641.9,4.743,4.743,0,0,0,.9,5.641ZM5.128,7.692V5.128a.513.513,0,0,1,1.025,0V7.692a.513.513,0,1,1-1.025,0Zm0-4.1a.513.513,0,1,1,.513.513A.513.513,0,0,1,5.128,3.59Z" transform="translate(2.051 2.051)" />
-                            </g>
-                        </svg>
-                    </div>
                 </div>
             </div>
         </div>
