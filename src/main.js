6f5426ad17a627a0b917e511ea5bc0abf1081771
Jafar <tljafer@gmail.com>
2023-01-20 10:16:13 +0400

    removed scss dependency

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import FloatingVue from "floating-vue";
 import "floating-vue/dist/style.css";
 import "simplebar/dist/simplebar.min.css";
-import "./assets/scss/style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
 import floatingOptions from "./helper/floating/options";
 
 Vue.config.productionTip = false;
